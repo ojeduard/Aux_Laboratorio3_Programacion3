7b74e5200bdd9fa0da3c0df3cf2cda97a6885ab5
Eduardo <eduardoaojedap@gmail.com>
2022-09-20 01:06:47 -0600

    Cambios

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -4,9 +4,15 @@ import Model.*;
 import View.*;
 
 public class Controller {
+    EmployeeManager employeeList = new EmployeeManager();
     public void startApp(){
         // Viewing and Controlling the application's GUI
         dbGUI view = new dbGUI();
         view.setVisible(true);
+    }
+
+    public void addingEmployees(String id, String name, String phone, String salary, String dptoCode){
+        employeeList.add(new Employee(id, name, Integer.parseInt(phone), Double.parseDouble(salary), dptoCode));
+        employeeList.createXML("Employees.xml");
     }
 }
--- src/View/dbGUI.java
+++ src/View/dbGUI.java
@@ -4,6 +4,8 @@ import Controller.Controller;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class dbGUI extends JFrame{
     private JTabbedPane tabPane;
@@ -19,12 +21,20 @@ public class dbGUI extends JFrame{
     private JTable table1;
 
     private  Controller controller = new Controller();
+    EmployeeView employeeView = new EmployeeView();
+
 
     public dbGUI(){
         setContentPane(this.main);
         setSize(600, 300);
         setVisible(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+        addEmployeeButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                employeeView.setVisible(true);
+            }
+        });
     }
 
 
